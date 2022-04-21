849851f18d92c282e57ececdb42fff86333a6571
React/ReactNative developer <67072920+DevWithReact@users.noreply.github.com>
2022-04-20 21:50:29 -0700

    Merge branch 'dataset-update-interval' into datset-calendar

--- lib/Widget/DataSetView.php
+++ lib/Widget/DataSetView.php
@@ -721,7 +721,7 @@ class DataSetView extends ModuleWidget
                 'widgetId' => $this->getWidgetId()
             ])
             ->appendJavaScript('
-                var dsTriggerTimer = 0;
+                let dsTriggerTimer = 0;
                 function setThresholdColor(){
                     let initalThreshold ='.$this->getOption('threshold').';
                     Object.keys(initalThreshold).reverse().forEach(function(key){
@@ -749,11 +749,25 @@ class DataSetView extends ModuleWidget
                         let data = res.data;
                         if(res.success) {
                             if (data == "false") {
+                                dsTriggerTimer && clearInterval(dsTriggerTimer);
                                 if (typeof parent.previewActionTrigger == "function"){
                                     parent.previewActionTrigger("/remove", {id: options.widgetId});
-                                    dsTriggerTimer && clearInterval(dsTriggerTimer);
+                                } else {
+                                    xiboIC.expireNow();
                                 }
                             }
+                        }
+                    });
+                }
+                function updateTableData() {
+                    $.ajax({
+                        type: "get",
+                        url: "'.$this->urlFor('module.getDataSetLive', ['regionId' => $this->region->regionId, 'id' => $this->widget->widgetId]).'",
+                    }).done(function(res) {
+                        let data = res.data;
+                        if(res.success) {
+                            $("#content").html(data.html);
+                            setThresholdColor();
                         }
                     });
                 }
@@ -765,10 +779,10 @@ class DataSetView extends ModuleWidget
                     (xiboIC.checkVisible()) ? runOnVisible() : xiboIC.addToQueue(runOnVisible);
 
                     setInterval(() => {
-                        window.location.reload();
+                        updateTableData();
                     }, options.updatesInterval * 1000);
                     
-                    if (options.enableCustomTrigger) {
+                    if (options.enableCustomTrigger === "1") {
                         dsTriggerTimer = setInterval(() => {
                             checkCustomTrigger();
                         }, 2000);
@@ -801,7 +815,7 @@ class DataSetView extends ModuleWidget
      * @throws \Xibo\Support\Exception\DuplicateEntityException
      * @throws \Xibo\Support\Exception\GeneralException
      */
-    private function dataSetTableHtml($displayId = 0)
+    public function dataSetTableHtml($displayId = 0)
     {
         // Show a preview of the data set table output.
         $dataSetId = $this->getOption('dataSetId');
--- lib/routes-web.php
+++ lib/routes-web.php
@@ -207,7 +207,7 @@ $app->group('', function(\Slim\Routing\RouteCollectorProxy $group) {
 })->addMiddleware(new FeatureAuth($app->getContainer(), ['layout.modify']));
 
 $app->get('/playlist/widget/calendar-items/{regionId}/{id}', ['\Xibo\Controller\Module','getCalendarItems'])->setName('module.getCalendarItems');
-
+$app->get('/playlist/widget/dataset-live/{regionId}/{id}', ['\Xibo\Controller\Module','getDataSetLive'])->setName('module.getDataSetLive');
 //
 // playlists
 //
